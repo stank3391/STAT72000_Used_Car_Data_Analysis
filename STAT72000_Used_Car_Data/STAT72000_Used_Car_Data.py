6a00d37072fcb7f06f9acc3aad83e5766bd8376c
Justin Schulz <Jschulz8138@conestogac.on.ca>
2024-04-18 16:03:56 -0400

    stuff

--- STAT72000_Used_Car_Data/STAT72000_Used_Car_Data.py
+++ STAT72000_Used_Car_Data/STAT72000_Used_Car_Data.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import warnings
 import plotly.express as px
 from plotly.subplots import make_subplots
+from scipy import stats
 import os
 
 warnings.filterwarnings('ignore')
@@ -21,10 +22,9 @@ except:
     data = pd.read_csv("./DataSet/Car details v3.csv")
 
 data.describe()
-
 data.head()
-data.isnull().sum()
 
+data.isnull().sum()
 data = data.dropna(axis=0)
 
 def convertToNumber(s:str):
@@ -34,68 +34,80 @@ def convertToNumber(s:str):
             d += i
     return eval(d)
 
-
 data["mileage"] = data["mileage"].apply(convertToNumber)
 data["engine"] = data["engine"].apply(convertToNumber)
 data["max_power"] = data["max_power"].apply(convertToNumber)
 
 data.head()
 data.isnull().sum()
-data = data.drop(['seller_type','torque','name','owner'],axis=1)
+data = data.drop(['torque','name','owner'],axis=1) #drop unused data
 data.head()
-data.fuel.unique()
-data_new = pd.get_dummies(data=data, columns=['fuel'], drop_first=True, dtype=int)
-data_new.head()
-data_new['transmission'] = data_new['transmission'].replace({'Automatic': 1, 'Manual': 0})
-data_new.head()
-#data_new['fuel'] = data_new['fuel'].replace({'fuel_Diesel': 2, 'fuel_LPG': 1, 'fuel_Petrol': 0})
-#data_new.head()
-
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
-mmScaler = MinMaxScaler()
-mmScaler_y = MinMaxScaler()
-
-label_enc = LabelEncoder()
-
-x = data_new[['year', 'km_driven','transmission', 'mileage', 'engine', 'max_power', 'seats', 'fuel_Diesel', 'fuel_LPG', 'fuel_Petrol']].values
-y = data_new[['selling_price']].values
-x[:,0] = label_enc.fit_transform(x[:,0])
-x = mmScaler.fit_transform(x)
-y = mmScaler_y.fit_transform(y)
-
+data['transmission'] = data['transmission'].replace({'Automatic': 1, 'Manual': 0}) #convert string to number
+data.head()
+data['seller_type'] = data['seller_type'].replace({'Trustmark Dealer' : 2, 'Individual': 1, 'Dealer': 0}) #convert string to number
+data.head()
+data['fuel'] = data['fuel'].replace({'LPG': 3, 'CNG': 2, 'Diesel': 1, 'Petrol': 0}) #convert string to number
+data.head()
 
 #### ACTUAL CODE HEREEEEEEE REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 
-# for column in data_new.columns:
-#     print(column + "\n")
-#     df = pd.DataFrame(data_new)
-#     print("Mean: ")
-#     print(df[column].mean());
-#     print("Median: ")
-#     print(df[column].median());
-#     print("Max: ")
-#     print(df[column].max());
-#     print("Min: ")
-#     print(df[column].min());
-#     print("Standard Deviation: ")
-#     print(df[column].std());
-#     print("Count: ")
-#     print(df[column].count());
-#     print("\n\n\n")
+#problem is shared database, create new dataframe per data column
+#create a data frame for each column and populate that dataframe with the trimmed data
+#put the trimmed data frame into the dataframes dict
+dataframes = []
+for column in data.columns:
+    df = pd.DataFrame(data[column])
+    q_low = df[column].quantile(0.05)
+    q_hi  = df[column].quantile(0.95)
+    dataframes.append(df[(df[column] < q_hi) & (df[column] > q_low)])
+    
 
-# for column in data_new.columns:
-#     sns.scatterplot(data=data_new, x=column, y='selling_price')
-#     plt.title(f"Scatter Plot between {column} and selling_price")
-#     plt.show()
+for column in data.columns:
+    df = pd.DataFrame(data[column])
 
-# for column in data_new.columns:
-#     testValues = data_new[column].value_counts()
-#     plt.pie(testValues, labels=testValues.index)    
-#     plt.title(f"Percentages of {column}")
-#     plt.show()
+    print(column + "\n")
+    print("Mean: ")
+    print(df[column].mean());
+    print("Median: ")
+    print(df[column].median());
+    print("Max: ")
+    print(df[column].max());
+    print("Min: ")
+    print(df[column].min());
+    print("Standard Deviation: ")
+    print(df[column].std());
+    print("Count: ")
+    print(df[column].count());
+    print("Correlation: ")
+    print(df[column].corr(df['selling_price']))
+    print("\n\n\n")
 
-# for column in data_new.columns:
-#       sns.histplot(data=data_new, x=column)
-#       plt.title(f"Histogram for {column}")
-#       plt.show()
+for column in data.columns:
+    df = pd.DataFrame(data)
+    # q_low = df[column].quantile(0.05)
+    # q_hi  = df[column].quantile(0.95)
+    # df = df[(df[column] < q_hi) & (df[column] > q_low)]
+    sns.scatterplot(data=df, x=column, y='selling_price')
+    plt.title(f"Scatter Plot between {column} and selling_price")
+    plt.show()
+
+for column in data.columns:
+    df = pd.DataFrame(data)
+    # q_low = df[column].quantile(0.05)
+    # q_hi  = df[column].quantile(0.95)
+    # df = df[(df[column] < q_hi) & (df[column] > q_low)]
+    testValues = df[column].value_counts()
+    plt.pie(testValues, labels=testValues.index, autopct='%1.0f%%', pctdistance=0.8, labeldistance=1.1)    
+    plt.title(f"Distribution of {column} by count.")
+    #plt.legend()
+    plt.show()
+
+for column in data.columns:
+    df = pd.DataFrame(data)
+    # q_low = df[column].quantile(0.05)
+    # q_hi  = df[column].quantile(0.95)
+    # df = df[(df[column] < q_hi) & (df[column] > q_low)]
+    sns.histplot(data=df, x=column)
+    plt.title(f"Distribution of {column}")
+    plt.show()
 
